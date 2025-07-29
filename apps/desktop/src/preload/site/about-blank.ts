e46b1ef6c5a544b930384a2152e3590432d28c15
biggerstar <1458048116@qq.com>
2025-07-30 05:30:45 +0800

    style: 添加使用技巧提示

--- apps/desktop/src/preload/site/about-blank.ts
+++ apps/desktop/src/preload/site/about-blank.ts
@@ -1,6 +1,16 @@
 
 export function useAboutBlankPage() {
   window.addEventListener("load", () => {
+    const tipContaner = document.createElement('div');
+    tipContaner.style.marginTop = '50px';
+    tipContaner.innerHTML = `
+        <p>使用技巧:</p>
+
+        <p>1. shift + 鼠标左键点击商品可打开新窗口</p>
+        <p>2. 快捷键 [ Ctrl + E ] 可以快速 打开 或 关闭 采集浏览器</p>
+    `;
+    document.body.appendChild(tipContaner);
+
     const tooltip = document.createElement('div');
     tooltip.style.position = 'fixed';
     tooltip.style.bottom = '100px';
